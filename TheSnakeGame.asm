073fdc7acf274e54dcfe35c456aca9f09134ecef
Spidy_Sain <sk.sudad.hossain@g.bracu.ac.bd>
2025-09-05 09:45:41 +0600

    TheSnakeGame.asm

--- TheSnakeGame.asm
+++ TheSnakeGame.asm
@@ -13,7 +13,6 @@ right db 4dh
 up    db 48h 
 down  db 50h 
 
-
 ;Initial movement direction 
 current_direction db 0
 
@@ -25,6 +24,13 @@ y_coor db 0
 foodx    db 0
 foody    db 0
 score     db 0
+
+;Score track
+high_score    db 0           
+speed_counter db 15         
+base_speed    db 30          
+return_msg db 10,13,13,10,10,13,9,9,9, "Press any key to return to MENU...", '$'
+
               
 main db 9,9, " |__   __| |           / ____|           | |        ",13,10  
      db 9,9, "    | |  | |__   ___  | (___  _ __   __ _| | _____  ",13,10
@@ -48,7 +54,7 @@ main db 9,9, " |__   __| |           / ____|           | |        ",13,10
 	 db 10h,"Arena2 (2) ",26," Arena size: 50x19",13,10
 	 db 10h,"Arena3 (3) ",26," Arena size: 50x16",13,10
 	    
-	 db 15h," Choose an arena: ", "$"
+	 db 10h," Choose an arena: ", "$"
 
 	 
 big    db 9,9,"ARENA 1 (50x22)",13,10,"$"  
@@ -62,7 +68,8 @@ border3          db 0dh,0ah,0c8h,0cdh,0cdh,0cdh,0cdh,0cdh,0cdh,0cdh,0cdh,0cdh,0c
 info1  db  "---The Snake Game---$"
 info2  db 16," Scoreboard $"
 info3  db 16," Score: $"
-info4  db 16,"Good Luck!",2,"$"
+info4  db 16," High Score: $"        
+info5  db 16,"Good Luck!",2,"$"
 
  
 
@@ -73,13 +80,16 @@ gameover              db 13,10,10,10,10,10,10,10,9,9,"  ___   __   _  _  ____   
            
               db 0ah,0ah,09h,09h,09h, "Oops... Looks like you lost! Better luck next time", 0dh,0ah 
               db 0ah,09h,09h,09h,"   Your Score: ",0dh,0ah   
+              db 0ah,09h,09h,09h,"   High Score: ",0dh,0ah    
               
               db 0ah,0ah,0ah,09h,09h,"  --- Game created by: Group 5 ---","$" 
               
               
 
 ;-------------------------------------------------- MENU -----------------------------------------------
-menu:
+menu:   
+mov ax, 03h   
+int 10h
 
 lea dx,main
 mov ah,9h
@@ -110,35 +120,50 @@ ret
 ;---------------------------ARENA 1-------------------------------
 arena1:
 
-lea dx,big
-mov ah,9
-int 21h
+    lea dx,big
+    mov ah,9
+    int 21h
 
-mov ah,9
-lea dx,border1
-int 21h
+    mov ah,9
+    lea dx,border1
+    int 21h
 
-mov cx,20
+    mov cx,20
 table1:   
+    lea dx,border2
+    mov ah,9      
+    int 21h
+    loop table1
 
-lea dx,border2
-mov ah,9      
-int 21h
-loop table1
+    lea dx,border3
+    mov ah,9      
+    int 21h 
 
-lea dx,border3
-mov ah,9      
-int 21h 
+    call table
+    mov cx,1
+    mov byte ptr [y_coor],22
 
-call table
-mov cx,1
-;mov y_coor,22
-;add y_coor,1 
-mov byte ptr [y_coor],22
+    
+    mov byte ptr [speed_counter], 10
+    mov byte ptr [base_speed], 10
+
+   
+    mov byte ptr [score], 0
+    mov word ptr [size], 3
+    mov byte ptr [grow], 0
+
+    ; Print high score immediately after drawing table
+    mov dl,72
+    mov dh,14
+    mov ah,02h
+    int 10h
+    mov al,[high_score]
+    mov ah,0
+    call print_num
+
+    jmp game
 
 
-
-jmp game 
  
  
 ;---------------------------ARENA 2-------------------------------         
@@ -167,10 +192,10 @@ int 21h
 
 call table
 mov cx,1
-;mov y_coor,19
-;add y_coor,1 
 mov byte ptr [y_coor],19
 
+mov byte ptr [speed_counter], 10
+mov byte ptr [base_speed], 10
 
 jmp game
 
@@ -201,10 +226,10 @@ int 21h
 
 call table
 mov cx,1
-;mov y_coor,14
-;add y_coor,1 
 mov byte ptr [y_coor],16
 
+mov byte ptr [speed_counter], 10
+mov byte ptr [base_speed], 10
 
 jmp game 
 
@@ -221,6 +246,23 @@ call food
 Game:
 cmp cx,1
 je coor
+
+
+dec byte ptr [speed_counter]     ;Speed Control
+cmp byte ptr [speed_counter], 0
+jne skip_movement          
+
+; Reset speed counter based on current score (higher score = faster)
+mov al, [score]            
+mov bl, [base_speed]       
+sub bl, al                
+cmp bl, 1                 
+jge set_speed
+mov bl, 1                  
+set_speed:
+mov [speed_counter], bl
+
+
 mov dx,snake[0]
 mov ah,2
 int 10h
@@ -232,15 +274,13 @@ mov cx,1
 mov ah,9
 int 10h
 
-;Creating a tail for the snake
-; tail = snake[(s_size*2) - 2]
-;Creating a tail for the snake
-; ---- Move first ----
+;--------------------------------------Creating a tail for the snake-----------------------------------------------
+
 call snake_move
 
-; ---- Now compute tail index ----
+
 mov bx,[size]
-dec bx                 ; use (size-1) always, old size tail
+dec bx               
 add bx, bx 
 sub bx,2
 mov ax,[snake+bx]
@@ -250,7 +290,7 @@ mov [tail],ax
 ; --------------------------------------Hide the old tail (skip once if we just ate)-----------------------------
 mov al,[grow]
 cmp al,0
-jne reduce_grow   ; if grow > 0, skip tail erase once
+jne reduce_grow   
 
 ; erase tail
 mov dx,[tail]
@@ -265,10 +305,7 @@ jmp key_check
 
 
 reduce_grow:
-dec byte ptr [grow]   ; decrease grow counter
-
-   
-;after_tail_hide
+dec byte ptr [grow]   
 
 key_check:
    
@@ -283,6 +320,22 @@ key_check:
    mov [current_direction],ah 
    
 no_key:
+mov cx,0
+jmp game
+
+
+skip_movement:
+   mov ah,1
+   int 16h
+   je no_key_skip
+   
+   mov ah,0
+   int 16h
+   cmp al,1bh
+   je gameover_screen
+   mov [current_direction],ah 
+   
+no_key_skip:
 mov cx,0
 jmp game
 
@@ -306,55 +359,83 @@ cmp ax,snake_max_length
 jge no_len
 inc ax
 mov [size],ax
-mov byte ptr [grow], 1   ; <-- grow exactly 1 block
+mov byte ptr [grow], 1   ;
 
 
 no_len:
-mov dl,66
+;------------------------ Display current score ------------------------------------
+mov dl,72
 mov dh,12
 mov ah,02h
 int 10h
 mov al,[score]        
 mov ah,0
 call print_num 
-call food
-ret
-;----------------------
-    ; Debug print Y
-    mov ah, 2
-    mov dl, dh
-    add dl, '0'
-    int 21h
 
-    ; Debug print separator
-    mov dl, ' '
-    int 21h
 
-    ; Debug print y_coor
-    mov dl, [y_coor]
-    add dl, '0'
-    int 21h
-;---------------------
-gameover_screen:
+mov al, [score]
+cmp al, [high_score]
+jle no_new_high_score
+mov [high_score], al       
 
-mov al,03h
-mov ah,0
-int 10h
+no_new_high_score:
 
-mov ah,9
-lea dx, gameover 
-int 21h
-
-mov dl,43
-mov dh,15
+mov dl,72                  
+mov dh,14                 
 mov ah,02h
 int 10h
-
-mov al,[score]
+mov al,[high_score]        
 mov ah,0
-call print_num
-int 20h
+call print_num 
 
+call food
+ret
+
+gameover_screen:
+
+    mov al,03h
+    mov ah,0
+    int 10h
+
+    mov ah,9
+    lea dx, gameover 
+    int 21h
+
+    ;----------------------- Display final score ------------------------------
+    mov dl,72                  
+    mov dh,15
+    mov ah,02h
+    int 10h
+
+    mov al,[score]
+    mov ah,0
+    call print_num
+
+    ; ------------------ Save last score into high_score unconditionally ------
+    mov al,[score]
+    mov [high_score], al
+
+    
+    mov dl,72                  
+    mov dh,16
+    mov ah,02h
+    int 10h
+
+    mov al,[high_score]
+    mov ah,0
+    call print_num
+
+    ; -----------Prompt and wait for a key, then go back to menu (do NOT terminate program) ---------------
+    lea dx, return_msg
+    mov ah,9
+    int 21h
+
+    mov ah,0
+    int 16h      
+    jmp menu
+
+
+;--------------------------------------------Procedures-------------------------------------------------------
 print_num proc
     push ax
     push bx
@@ -384,6 +465,8 @@ print_num proc
     pop ax
     ret
 print_num endp
+
+;----------------------------------------FOOD PROC--------------------------------------------------
 
 food proc
     
@@ -431,7 +514,7 @@ food proc
     ret
 food endp
 
-
+;----------------------------------------Table PROC--------------------------------------------------
 
 table proc 
     ;1st info
@@ -450,7 +533,7 @@ table proc
     lea dx,info2
     mov ah,9
     int 21h 
-    ;3rd
+    ;3rd - Current Score
     mov dl,57
     mov dh,12 
     mov ah,2
@@ -458,16 +541,34 @@ table proc
     lea dx,info3
     mov ah,9
     int 21h   
-    ;4th
-    mov dl,58
-    mov dh,15 
+    ;4th -  High Score
+    mov dl,57
+    mov dh,14 
     mov ah,2
     int 10h
     lea dx,info4
     mov ah,9
+    int 21h
+    ; Display initial high score
+    mov dl,72
+    mov dh,14
+    mov ah,02h
+    int 10h
+    mov al,[high_score]        
+    mov ah,0
+    call print_num 
+    ;5th - Good Luck message
+    mov dl,58
+    mov dh,17 
+    mov ah,2
+    int 10h
+    lea dx,info5
+    mov ah,9
     int 21h 
     ret
-table endp 
+table endp   
+
+;----------------------------------------Snake Logic PROC--------------------------------------------------
 
 snake_move proc
     mov bx, [size]
@@ -517,7 +618,7 @@ move_right:
 move_up:
     mov dx, [snake]
     dec dh
-    cmp dh, 3           ; top border value
+    cmp dh, 3           
     jl gameover_screen
     mov [snake], dx
     jmp after_move
@@ -534,28 +635,28 @@ move_down:
 after_move:
     mov ax, [size]
     cmp ax, 4
-    jl after_collision_check  ; skip self-collision check if snake too short
+    jl after_collision_check  
 
-    mov ax, snake[0]          ; head position
+    mov ax, snake[0]          
     mov cx, [size]
-    dec cx                    ; number of body segments to check
+    dec cx                   
     
-    mov bx, 2                 ; start offset at second segment
+    mov bx, 2                 
 
 check_loop:
     cmp cx, 0
     je after_collision_check
 
-    mov dx, snake[bx]         ; current segment position
+    mov dx, snake[bx]         
     cmp ax, dx
-    je gameover_screen        ; collision detected
+    je gameover_screen        
 
-    add bx, 2                 ; advance to next segment (word size)
+    add bx, 2                 
     dec cx
     jmp check_loop
 
 after_collision_check:
-    ; Check if snake head is on food
+    
     mov ax, snake[0]
     cmp ah, foody
     jne no_food
@@ -563,7 +664,6 @@ after_collision_check:
     cmp al, foodx
     jne no_food
 
-    ; Snake ate the food
     call atributeFood
 
 no_food:
@@ -574,3 +674,4 @@ snake_move endp
                    
 
 END
+
